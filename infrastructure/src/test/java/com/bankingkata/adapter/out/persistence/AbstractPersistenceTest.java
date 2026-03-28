c554ce9623234a53a15a78f7f89e7f65365f1a06
ba-amanou <218820324+ba-amanou@users.noreply.github.com>
2026-03-28 23:35:20 +0100

    feat(infrastructure): add persistence tests with testcontainers

---
+++ infrastructure/src/test/java/com/bankingkata/adapter/out/persistence/AbstractPersistenceTest.java
@@ -0,0 +1,27 @@
+package com.bankingkata.adapter.out.persistence;
+
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+public class AbstractPersistenceTest {
+    
+    @Container
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:15");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+}
