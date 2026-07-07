10680d0cabe1904176ffd05fd0a37b4f410a8f8d
ba-amanou <218820324+ba-amanou@users.noreply.github.com>
2026-07-07 23:59:10 +0200

    fix(application): make deposit/withdraw atomic

--- application/src/main/java/com/bankingkata/service/DepositMoneyService.java
+++ application/src/main/java/com/bankingkata/service/DepositMoneyService.java
@@ -8,6 +8,7 @@ import com.bankingkata.port.in.DepositMoneyUseCase;
 import com.bankingkata.port.out.LoadAccountPort;
 import com.bankingkata.port.out.SaveAccountPort;
 import com.bankingkata.port.out.SaveTransactionPort;
+import com.bankingkata.port.out.UnitOfWork;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,19 +18,22 @@ public class DepositMoneyService implements DepositMoneyUseCase {
     private final SaveAccountPort saveAccountPort;
     private final LoadAccountPort loadAccountPort;
     private final SaveTransactionPort saveTransactionPort;
+    private final UnitOfWork unitOfWork;
 
     @Override
     public Account deposit(String accountId, Money amount) {
-        Account account = loadAccountPort.load(accountId)
-            .orElseThrow(() -> new AccountNotFoundException(accountId));
+        return unitOfWork.execute(() -> {
+            Account account = loadAccountPort.load(accountId)
+                .orElseThrow(() -> new AccountNotFoundException(accountId));
 
-        account.deposit(amount);
-        Transaction transaction = Transaction.deposit(accountId, amount);
+            account.deposit(amount);
+            Transaction transaction = Transaction.deposit(accountId, amount);
 
-        saveAccountPort.save(account);
-        saveTransactionPort.save(transaction);
+            saveAccountPort.save(account);
+            saveTransactionPort.save(transaction);
 
-        return account;
+            return account;
+        });
     }
 
     
