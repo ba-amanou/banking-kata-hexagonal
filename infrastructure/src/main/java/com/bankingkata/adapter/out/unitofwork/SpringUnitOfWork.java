1c7c35f1035fb525aed3c47178c8589402425548
ba-amanou <218820324+ba-amanou@users.noreply.github.com>
2026-07-12 23:19:24 +0200

    feat(account):  map concurrent modification conflicts to a 409 response

--- infrastructure/src/main/java/com/bankingkata/adapter/out/unitofwork/SpringUnitOfWork.java
+++ infrastructure/src/main/java/com/bankingkata/adapter/out/unitofwork/SpringUnitOfWork.java
@@ -2,9 +2,11 @@ package com.bankingkata.adapter.out.unitofwork;
 
 import java.util.function.Supplier;
 
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import com.bankingkata.exception.ConcurrentAccountModificationException;
 import com.bankingkata.port.out.UnitOfWork;
 
 import lombok.RequiredArgsConstructor;
@@ -17,7 +19,11 @@ public class SpringUnitOfWork implements UnitOfWork {
 
     @Override
     public <T> T execute(Supplier<T> action) {
-       return transactionTemplate.execute(status -> action.get());
+        try {
+            return transactionTemplate.execute(status -> action.get());   
+        } catch (OptimisticLockingFailureException e) {
+            throw new ConcurrentAccountModificationException();
+        }
     }
  
 }
